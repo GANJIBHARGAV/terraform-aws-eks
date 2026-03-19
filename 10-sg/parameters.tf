183dc262a40aa7ade1dc1ef30c8a87c3d290f3c0
Bhargav Ganji <bhargavganji08.com>
2026-03-19 18:38:32 +0530

    terraform

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -21,10 +21,12 @@ resource "aws_ssm_parameter" "bastion_sg_id" {
   name  = "/${var.project}/${var.environment}/bastion_sg_id"
   type  = "String"
   value = module.bastion.sg_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "vpn_sg_id" {
   name  = "/${var.project}/${var.environment}/vpn_sg_id"
   type  = "String"
   value = module.vpn.sg_id
+  overwrite = true
 }
